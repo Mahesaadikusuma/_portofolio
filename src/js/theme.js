ae9e1ef5f90e9e015f92539d01f22f40988106c6
Mahesaadikusuma <mahesaadikuzuma@gmail.com>
2023-12-07 21:17:39 +0700

    dark mode

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,18 +1,86 @@
-const theme = document.getElementById('dark');
-const button = document.getElementById('button');
+// DESKTOP
+// Dark icon
+const button = document.getElementById('button-theme');
+const darkD = document.getElementById("darkD");
+// light icon
+const lightIcon = document.getElementById("lightD") 
+// DESKTOP
+
 const html = document.querySelector('html')
-console.log(theme)
+
 
 
 // mobile
-theme.addEventListener('click', () => {
-     html.classList.toggle('dark')
+const toggleMobile = document.getElementById('toggleMobile')
+const MobileLight = document.getElementById('light-mobile')
+const MobileDark = document.getElementById('dark-mobile')
+toggleMobile.addEventListener('click', () => {
+     toggleDarkMode();
 })
 
 // desktop
 button.addEventListener('click', () => {
-     html.classList.toggle('dark')
+     toggleDarkMode();
 })
+
+// function updateIcon() {
+//      const isDarkMode = html.classList.contains('dark');
+//      if (isDarkMode) {
+//           console.log('selamat anda is dark')
+//           darkD.classList.remove('hidden');
+//           lightIcon.classList.add('hidden');
+
+//           // Mobile
+//           MobileDark.classList.remove('hidden');
+//           MobileLight.classList.add('hidden');
+
+//           localStorage.theme = 'dark'
+
+
+//      } else {
+//           console.log('selamat anda is light')
+//           darkD.classList.add('hidden');
+//           lightIcon.classList.remove('hidden');
+
+//           // Mobile
+//           MobileDark.classList.add('hidden');
+//           MobileLight.classList.remove('hidden');
+          
+//           localStorage.theme = "light";
+//      }
+
+//  }
+
+function toggleDarkMode() {
+     html.classList.toggle('dark');
+     updateIcon();
+     saveThemePreference();
+ }
+
+function updateIcon() {
+     const isDarkMode = html.classList.contains('dark');
+     
+     darkD.classList.toggle('hidden', !isDarkMode);
+     lightIcon.classList.toggle('hidden', isDarkMode);
+
+     MobileDark.classList.toggle('hidden', isDarkMode);
+     MobileLight.classList.toggle('hidden', !isDarkMode);
+
+     localStorage.theme = isDarkMode ? 'dark' : 'light';
+}
+
+function saveThemePreference() {
+     const isDarkMode = html.classList.contains('dark');
+     localStorage.theme = isDarkMode ? 'dark' : 'light';
+ }
+
+ if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+     // document.documentElement.classList.add('dark')
+     html.classList.add('dark');
+   } else {
+     // document.documentElement.classList.remove('dark')
+     html.classList.remove('dark');
+   }
 
 // Hamburger button
 const hamburger = document.getElementById('hamburger')
